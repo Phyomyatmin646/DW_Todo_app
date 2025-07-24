e245aaa26103fca151e0ed32eb1ff1da8dd50f06
Phyomyatmin646 <phyomyatmin646@gamil.com>
2025-07-24 19:28:08 +0630

    first

---
+++ myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import TodoModel
+# Create your views here.
+def Todolist(request):
+    todo=TodoModel.objects.all()
+    contex={
+        'todo':todo
+    }
+    return render(request,'index.html',contex)
